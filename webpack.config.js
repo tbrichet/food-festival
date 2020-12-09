5884a87d3547e5240388d7f8155af221bf30369c
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-12-09 13:47:45 -0700

    image webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,47 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // Create the main configuration object within our file
 // The options within this object tell webpack what to do
-module.exports = {
-    // Entry Point
-    entry: './assets/js/script.js',
+const config = {
+    // Entry Points
+    entry: {
+      app: "./assets/js/script.js",
+      events: "./assets/js/events.js",
+      schedule: "./assets/js/schedule.js",
+      tickets: "./assets/js/tickets.js"
+    },
     // Output the bundled code into a folder that we specify
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-      },
+      filename: "[name].bundle.js",
+      path: __dirname + "/dist",
+    },
+    // Add modules
+    module: {
+      rules: [
+        // Run file-loader for images
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name(file) {
+                  return '[path][name].[ext]';
+                },
+                publicPath: function(url) {
+                  return url.replace('../', '/assets/');
+                }
+              }
+            },
+            {
+              loader: 'image-webpack-loader'
+            }
+          ]
+        }
+      ]
+    },
     // Add plugins
     plugins: [
       // run jQuery
@@ -18,8 +49,14 @@ module.exports = {
         $: "jquery",
         jQuery: "jquery"
       }),
+      // Run BundleAnalyzer
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      })
     ],
     // The mode in which we want webpack to run (default: production mode)
     mode: 'development'
 };
+
+module.exports = config;
 
